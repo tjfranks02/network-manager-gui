ef7ccd715a6f2ae0ad0c904570be9fbb3d5454b7
Tyson Franks <tjfranks02@hotmail.com>
2023-10-15 00:07:17 +1100

    Added in dodgy placeholder header/navbar component.

---
+++ ui/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import css from "./styles.module.css";
+
+const Header = () => {
+  return (
+    <div>WOOHOO this is the header for the app! PErhaps a better name is navbar :-)</div>
+  );
+};
+
+export default Header;
--- ui/src/pages/Dashboard/Dashboard.tsx
+++ ui/src/pages/Dashboard/Dashboard.tsx
@@ -4,16 +4,20 @@ import Canvas from "../../components/EditorView/Canvas/Canvas";
 import ActiveElementPanel from "../../components/EditorView/ActiveElementPanel/ActiveElementPanel";
 import ElementSelectorPanel from "../../components/EditorView/ElementSelectorPanel/ElementSelectorPanel";
 
-import css from './styles.module.css';
+import css from "./styles.module.css";
+import Header from "../../components/Header/Header";
 
 const Dashboard = () => {
   const activeElement = useSelector((state: RootState) => state.activeElement.element);
 
   return (
     <div className={css.container}>
-      <ElementSelectorPanel />
-      <Canvas />
-      <ActiveElementPanel activeElement={activeElement} />
+      <Header />
+      <div className={css.editorContainer}>
+        <ElementSelectorPanel />
+        <Canvas />
+        <ActiveElementPanel activeElement={activeElement} />
+      </div>
     </div>
   );
 };
