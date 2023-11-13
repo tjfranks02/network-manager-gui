d1cbec10c8e2bdbe866df5100630686f7b68f375
Tyson Franks <tjfranks02@hotmail.com>
2023-11-13 23:45:53 +1100

    Changed active element panel to only display the first 6 characters of the active element id

--- src/components/editorView/ActiveElementPanel/index.tsx
+++ src/components/editorView/ActiveElementPanel/index.tsx
@@ -4,7 +4,7 @@ import css from "./styles.module.css";
 const ActiveElementPanel = ({ activeElement }: { activeElement: string }) => {
   return (
     <div className={css.container}>
-      <h3>{activeElement}</h3>
+      <h3>{activeElement ? activeElement.substring(0, 5) : null}</h3>
       {activeElement ? <p>This is the active element</p> : <p>No active element</p>}
       <EditableTextField>Editable text field</EditableTextField>
     </div>
